e9d72a871fd0a713c7a2f844ba2eecde828ea269
Adider27 <adidermawan81@gmail.com>
2022-01-08 02:06:08 -0800

    Menambahkan semua kode form pengembalian

--- Aplikasi_Laundry/Pengembalian.Designer.cs
+++ Aplikasi_Laundry/Pengembalian.Designer.cs
@@ -48,117 +48,120 @@ namespace Aplikasi_Laundry
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(202, 12);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(404, 23);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(159, 17);
+            this.label7.Size = new System.Drawing.Size(386, 31);
             this.label7.TabIndex = 71;
             this.label7.Text = "PENGEMBALIAN LAUNDRY";
             // 
             // btrefresh
             // 
-            this.btrefresh.Location = new System.Drawing.Point(132, 105);
-            this.btrefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btrefresh.Location = new System.Drawing.Point(264, 202);
+            this.btrefresh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btrefresh.Name = "btrefresh";
-            this.btrefresh.Size = new System.Drawing.Size(61, 27);
+            this.btrefresh.Size = new System.Drawing.Size(122, 52);
             this.btrefresh.TabIndex = 70;
             this.btrefresh.Text = "Refresh";
             this.btrefresh.UseVisualStyleBackColor = true;
+            this.btrefresh.Click += new System.EventHandler(this.btrefresh_Click);
             // 
             // btcari
             // 
-            this.btcari.Location = new System.Drawing.Point(542, 120);
-            this.btcari.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btcari.Location = new System.Drawing.Point(1084, 231);
+            this.btcari.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btcari.Name = "btcari";
-            this.btcari.Size = new System.Drawing.Size(52, 22);
+            this.btcari.Size = new System.Drawing.Size(104, 42);
             this.btcari.TabIndex = 69;
             this.btcari.Text = "Cari";
             this.btcari.UseVisualStyleBackColor = true;
+            this.btcari.Click += new System.EventHandler(this.btcari_Click);
             // 
             // txcari
             // 
-            this.txcari.Location = new System.Drawing.Point(471, 123);
-            this.txcari.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txcari.Location = new System.Drawing.Point(942, 237);
+            this.txcari.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txcari.Name = "txcari";
-            this.txcari.Size = new System.Drawing.Size(68, 20);
+            this.txcari.Size = new System.Drawing.Size(132, 31);
             this.txcari.TabIndex = 68;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(437, 127);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(874, 244);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.Size = new System.Drawing.Size(63, 25);
             this.label2.TabIndex = 67;
             this.label2.Text = "Cari :";
             // 
             // btdikembalikan
             // 
-            this.btdikembalikan.Location = new System.Drawing.Point(38, 105);
-            this.btdikembalikan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btdikembalikan.Location = new System.Drawing.Point(76, 202);
+            this.btdikembalikan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btdikembalikan.Name = "btdikembalikan";
-            this.btdikembalikan.Size = new System.Drawing.Size(81, 27);
+            this.btdikembalikan.Size = new System.Drawing.Size(162, 52);
             this.btdikembalikan.TabIndex = 66;
             this.btdikembalikan.Text = "Dikembalikan";
             this.btdikembalikan.UseVisualStyleBackColor = true;
+            this.btdikembalikan.Click += new System.EventHandler(this.btdikembalikan_Click);
             // 
             // dgvpengembalian
             // 
             this.dgvpengembalian.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvpengembalian.Location = new System.Drawing.Point(31, 162);
-            this.dgvpengembalian.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvpengembalian.Location = new System.Drawing.Point(62, 312);
+            this.dgvpengembalian.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvpengembalian.Name = "dgvpengembalian";
             this.dgvpengembalian.RowHeadersWidth = 62;
             this.dgvpengembalian.RowTemplate.Height = 28;
-            this.dgvpengembalian.Size = new System.Drawing.Size(563, 190);
+            this.dgvpengembalian.Size = new System.Drawing.Size(1126, 365);
             this.dgvpengembalian.TabIndex = 65;
             // 
             // tglkeluar
             // 
             this.tglkeluar.CustomFormat = "dd-MM-yyyy";
             this.tglkeluar.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.tglkeluar.Location = new System.Drawing.Point(461, 45);
-            this.tglkeluar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tglkeluar.Location = new System.Drawing.Point(922, 87);
+            this.tglkeluar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tglkeluar.Name = "tglkeluar";
-            this.tglkeluar.Size = new System.Drawing.Size(135, 20);
+            this.tglkeluar.Size = new System.Drawing.Size(266, 31);
             this.tglkeluar.TabIndex = 64;
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(458, 31);
-            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Location = new System.Drawing.Point(916, 60);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(91, 13);
+            this.label13.Size = new System.Drawing.Size(183, 25);
             this.label13.TabIndex = 63;
             this.label13.Text = "Tgl pengembalian";
             // 
             // txidcm
             // 
-            this.txidcm.Location = new System.Drawing.Point(126, 60);
-            this.txidcm.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txidcm.Location = new System.Drawing.Point(252, 115);
+            this.txidcm.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txidcm.Name = "txidcm";
-            this.txidcm.Size = new System.Drawing.Size(68, 20);
+            this.txidcm.Size = new System.Drawing.Size(132, 31);
             this.txidcm.TabIndex = 62;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 64);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(78, 123);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.Size = new System.Drawing.Size(167, 25);
             this.label1.TabIndex = 61;
             this.label1.Text = "Id cucian masuk";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(0, 1);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Location = new System.Drawing.Point(0, 2);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(61, 27);
+            this.button5.Size = new System.Drawing.Size(122, 52);
             this.button5.TabIndex = 60;
             this.button5.Text = "Back";
             this.button5.UseVisualStyleBackColor = true;
@@ -167,18 +170,18 @@ namespace Aplikasi_Laundry
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 146);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(62, 281);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 13);
+            this.label3.Size = new System.Drawing.Size(325, 25);
             this.label3.TabIndex = 72;
             this.label3.Text = "CUCIAN YANG BELUM DIAMBIL";
             // 
             // Pengembalian
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 361);
+            this.ClientSize = new System.Drawing.Size(1268, 694);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btrefresh);
@@ -192,7 +195,7 @@ namespace Aplikasi_Laundry
             this.Controls.Add(this.txidcm);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Pengembalian";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pengembalian";
--- Aplikasi_Laundry/Pengembalian.cs
+++ Aplikasi_Laundry/Pengembalian.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Aplikasi_Laundry
 {
@@ -23,10 +24,79 @@ namespace Aplikasi_Laundry
             this.Hide();
             fom.Show();
         }
-
-        private void Pengembalian_Load(object sender, EventArgs e)
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-CJNVJPUI;Initial Catalog=Laundry.v2;Integrated Security=True");
+        private void showdata()
         {
 
+            SqlCommand cmd = new SqlCommand("select * from Pengembalian", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvpengembalian.DataSource = dt;
+            con.Close();
+        }
+        private void Pengembalian_Load(object sender, EventArgs e)
+        {
+            showdata();
+        }
+
+        private void btdikembalikan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string berat = "";
+                string harga = "";
+                string id = "";
+                con.Open();
+                SqlCommand cmd1 = new SqlCommand("select * from Pengembalian where Id_cucian_masuk=@Id_cucian_masuk", con);
+                cmd1.Parameters.AddWithValue("@Id_cucian_masuk", txidcm.Text);
+                cmd1.ExecuteNonQuery();
+                SqlDataReader rd = cmd1.ExecuteReader();
+                rd.Read();
+                id = rd[1].ToString();
+                berat = rd[2].ToString();
+                harga = rd[3].ToString();
+                rd.Close();
+                con.Close();
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into Cucian_keluar values (@Id_cucian_masuk,@Id_pelanggan,@Berat,@Harga,@Tgl_keluar)", con);
+                cmd.Parameters.AddWithValue("@Id_cucian_masuk", txidcm.Text);
+                cmd.Parameters.AddWithValue("@Id_pelanggan", id);
+                cmd.Parameters.AddWithValue("@Berat", float.Parse(berat));
+                cmd.Parameters.AddWithValue("@Harga", float.Parse(harga));
+                cmd.Parameters.AddWithValue("@Tgl_keluar", tglkeluar.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("cucian dikembalikan");
+
+                SqlCommand cmd2 = new SqlCommand("delete Pengembalian where Id_pelanggan=@Id_pelanggan", con);
+                cmd2.Parameters.AddWithValue("@Id_pelanggan", id);
+                cmd2.ExecuteNonQuery();
+                showdata();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("tidak ada data cucian yang dikembalikan");
+            }
+        }
+
+        private void btcari_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Pengembalian where Id_cucian_masuk=@Id_cucian_masuk", con);
+            cmd.Parameters.AddWithValue("@Id_cucian_masuk", txcari.Text);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvpengembalian.DataSource = dt;
+            con.Close();
+        }
+
+        private void btrefresh_Click(object sender, EventArgs e)
+        {
+            showdata();
         }
     }
 }
