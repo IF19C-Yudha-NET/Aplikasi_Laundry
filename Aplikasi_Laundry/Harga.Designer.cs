ddc5a33cfe2a51e64859d2402be167568961ca07
yudhaaditama16 <yudhaaditama270@gmail.com>
2022-01-08 13:34:09 +0700

    menambahkan kode tetapkan harga

--- Aplikasi_Laundry/Harga.Designer.cs
+++ Aplikasi_Laundry/Harga.Designer.cs
@@ -51,118 +51,133 @@ namespace Aplikasi_Laundry
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(276, 20);
+            this.label2.Location = new System.Drawing.Point(184, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(258, 24);
+            this.label2.Size = new System.Drawing.Size(182, 17);
             this.label2.TabIndex = 73;
             this.label2.Text = "PENETAPAN HARGA LAUNDRY";
             // 
             // dgvharga
             // 
             this.dgvharga.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvharga.Location = new System.Drawing.Point(53, 306);
+            this.dgvharga.Location = new System.Drawing.Point(35, 199);
+            this.dgvharga.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvharga.Name = "dgvharga";
             this.dgvharga.RowHeadersWidth = 62;
             this.dgvharga.RowTemplate.Height = 28;
-            this.dgvharga.Size = new System.Drawing.Size(714, 121);
+            this.dgvharga.Size = new System.Drawing.Size(476, 79);
             this.dgvharga.TabIndex = 72;
             // 
             // txcsetrika
             // 
-            this.txcsetrika.Location = new System.Drawing.Point(232, 206);
+            this.txcsetrika.Location = new System.Drawing.Point(155, 134);
+            this.txcsetrika.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txcsetrika.Name = "txcsetrika";
-            this.txcsetrika.Size = new System.Drawing.Size(100, 26);
+            this.txcsetrika.Size = new System.Drawing.Size(68, 20);
             this.txcsetrika.TabIndex = 71;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 212);
+            this.label1.Location = new System.Drawing.Point(45, 138);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 70;
             this.label1.Text = "Harga cuci setrika";
             // 
             // txhwangi
             // 
-            this.txhwangi.Location = new System.Drawing.Point(232, 158);
+            this.txhwangi.Location = new System.Drawing.Point(155, 103);
+            this.txhwangi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txhwangi.Name = "txhwangi";
-            this.txhwangi.Size = new System.Drawing.Size(100, 26);
+            this.txhwangi.Size = new System.Drawing.Size(68, 20);
             this.txhwangi.TabIndex = 69;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(68, 164);
+            this.label8.Location = new System.Drawing.Point(45, 107);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(158, 20);
+            this.label8.Size = new System.Drawing.Size(107, 13);
             this.label8.TabIndex = 68;
             this.label8.Text = "Harga diberi pewangi";
             // 
             // txhpkilat
             // 
-            this.txhpkilat.Location = new System.Drawing.Point(232, 113);
+            this.txhpkilat.Location = new System.Drawing.Point(155, 73);
+            this.txhpkilat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txhpkilat.Name = "txhpkilat";
-            this.txhpkilat.Size = new System.Drawing.Size(100, 26);
+            this.txhpkilat.Size = new System.Drawing.Size(68, 20);
             this.txhpkilat.TabIndex = 67;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(68, 119);
+            this.label7.Location = new System.Drawing.Point(45, 77);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(129, 20);
+            this.label7.Size = new System.Drawing.Size(88, 13);
             this.label7.TabIndex = 66;
             this.label7.Text = "Harga paket kilat";
             // 
             // txhkg
             // 
-            this.txhkg.Location = new System.Drawing.Point(232, 69);
+            this.txhkg.Location = new System.Drawing.Point(155, 45);
+            this.txhkg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txhkg.Name = "txhkg";
-            this.txhkg.Size = new System.Drawing.Size(100, 26);
+            this.txhkg.Size = new System.Drawing.Size(68, 20);
             this.txhkg.TabIndex = 65;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(68, 75);
+            this.label6.Location = new System.Drawing.Point(45, 49);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 20);
+            this.label6.Size = new System.Drawing.Size(69, 13);
             this.label6.TabIndex = 64;
             this.label6.Text = "Harga per kg";
             // 
             // bthapus
             // 
-            this.bthapus.Location = new System.Drawing.Point(424, 190);
+            this.bthapus.Location = new System.Drawing.Point(283, 123);
+            this.bthapus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bthapus.Name = "bthapus";
-            this.bthapus.Size = new System.Drawing.Size(91, 42);
+            this.bthapus.Size = new System.Drawing.Size(61, 27);
             this.bthapus.TabIndex = 63;
             this.bthapus.Text = "Hapus";
             this.bthapus.UseVisualStyleBackColor = true;
             // 
             // btubah
             // 
-            this.btubah.Location = new System.Drawing.Point(521, 190);
+            this.btubah.Location = new System.Drawing.Point(347, 123);
+            this.btubah.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btubah.Name = "btubah";
-            this.btubah.Size = new System.Drawing.Size(91, 42);
+            this.btubah.Size = new System.Drawing.Size(61, 27);
             this.btubah.TabIndex = 62;
             this.btubah.Text = "Ubah";
             this.btubah.UseVisualStyleBackColor = true;
             // 
             // bttetapkan
             // 
-            this.bttetapkan.Location = new System.Drawing.Point(424, 142);
+            this.bttetapkan.Location = new System.Drawing.Point(283, 92);
+            this.bttetapkan.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bttetapkan.Name = "bttetapkan";
-            this.bttetapkan.Size = new System.Drawing.Size(188, 42);
+            this.bttetapkan.Size = new System.Drawing.Size(125, 27);
             this.bttetapkan.TabIndex = 61;
             this.bttetapkan.Text = "Tetapkan harga";
             this.bttetapkan.UseVisualStyleBackColor = true;
+            this.bttetapkan.Click += new System.EventHandler(this.bttetapkan_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(2, 2);
+            this.button5.Location = new System.Drawing.Point(1, 1);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(91, 42);
+            this.button5.Size = new System.Drawing.Size(61, 27);
             this.button5.TabIndex = 60;
             this.button5.Text = "Back";
             this.button5.UseVisualStyleBackColor = true;
@@ -171,17 +186,18 @@ namespace Aplikasi_Laundry
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(49, 283);
+            this.label3.Location = new System.Drawing.Point(33, 184);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(221, 20);
+            this.label3.Size = new System.Drawing.Size(149, 13);
             this.label3.TabIndex = 83;
             this.label3.Text = "HARGA YANG DITETAPKAN";
             // 
             // Harga
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(809, 474);
+            this.ClientSize = new System.Drawing.Size(539, 308);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dgvharga);
@@ -197,9 +213,11 @@ namespace Aplikasi_Laundry
             this.Controls.Add(this.btubah);
             this.Controls.Add(this.bttetapkan);
             this.Controls.Add(this.button5);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Harga";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Harga";
+            this.Load += new System.EventHandler(this.Harga_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvharga)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Aplikasi_Laundry/Harga.cs
+++ Aplikasi_Laundry/Harga.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Aplikasi_Laundry
 {
@@ -22,6 +23,38 @@ namespace Aplikasi_Laundry
             Form1 fom = new Form1();
             this.Hide();
             fom.Show();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-O365U80\SQLEXPRESS;Initial Catalog=Laundry.v2;Integrated Security=True");
+
+        private void showdata()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Harga", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvharga.DataSource = dt;
+            con.Close();
+        }
+
+        private void bttetapkan_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Harga values (@Harga_per_kg,@Harga_paket_kilat,@Harga_pewangi,@Harga_jenis_cuci)", con);
+            cmd.Parameters.AddWithValue("@Harga_per_kg", float.Parse(txhkg.Text));
+            cmd.Parameters.AddWithValue("@Harga_paket_kilat", float.Parse(txhpkilat.Text));
+            cmd.Parameters.AddWithValue("@Harga_pewangi", float.Parse(txhwangi.Text));
+            cmd.Parameters.AddWithValue("@Harga_jenis_cuci", float.Parse(txcsetrika.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("harga berhasil ditetapkan");
+            showdata();
+        }
+
+        private void Harga_Load(object sender, EventArgs e)
+        {
+            showdata();
         }
     }
 }
